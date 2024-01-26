168f737016facadc74996174f49ce9fe86fa52da
ENG-MARTIN <maartinsseba3@gmail.com>
2024-01-26 21:52:31 +0300

    Day 15

---
+++ dateSpinner.js
@@ -0,0 +1,6 @@
+{/* <span><select id="dayDropdown">                         */}
+
+    for (let i = 1; i <= 31; i++) {
+        document.write(`<option value="${i}">${i}</option>`);
+    }
+{/* </select></span> */}
